2fd49f853f4c41f3be19101ef269f229bb93ece8
Priscila <priscilasartori2@gmail.com>
2023-04-27 14:20:57 -0300

    feat: Adicionando a model e a interface de cars

---
+++ src/Interfaces/ICars.ts
@@ -0,0 +1,12 @@
+interface ICars {
+  id?: string;
+  model: string;
+  year: number;
+  color: string;
+  status?: boolean;
+  buyValue: number;
+  doorsQty: number;
+  seatsQty: number;
+}
+
+export default ICars;
---
+++ src/Models/CarsModel.ts
@@ -0,0 +1,31 @@
+import {
+  Model,
+  Schema,
+  model,
+  models,
+} from 'mongoose';
+import ICars from '../Interfaces/ICars';
+
+class CarsModel {
+  private schema: Schema;
+  private model: Model<ICars>;
+
+  constructor() {
+    this.schema = new Schema<ICars>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this.model = models.CarsModel || model('Cars', this.schema);
+  }
+
+  public async create(cars: ICars): Promise<ICars> {
+    return this.model.create({ ...cars });
+  }
+}
+
+export default CarsModel;

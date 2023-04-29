55668feb72ded1ce4379ad503c8f53f091bdb343
Priscila <priscilasartori2@gmail.com>
2023-04-29 16:35:38 -0300

    feat: Adicionando o service para listar motorcycles

--- src/Services/MotorcyclesServices.ts
+++ src/Services/MotorcyclesServices.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import MotorcyclesDomains from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcyclesModel from '../Models/MotorcyclesODM';
@@ -10,6 +11,25 @@ class MotorcyclesService {
       return new MotorcyclesDomains(motorcycle);
     }
     return null;
+  }
+
+  public async getAll() {
+    const motorcyclesModel = new MotorcyclesModel();
+    const motorcycles = await motorcyclesModel.getAll();
+    const motorcyclesArray = motorcycles.map((motorcycle) => {
+      if (motorcycle) return new MotorcyclesDomains(motorcycle);
+      return null;
+    }); 
+    return { status: 200, message: motorcyclesArray };
+  }
+
+  public async getById(id: string) {
+    const motorcyclesModel = new MotorcyclesModel();
+    if (!isValidObjectId(id)) return { status: 422, message: { message: 'Invalid mongo id' } };
+    const motorcycles = await motorcyclesModel.getById(id);
+    if (motorcycles === null) return { status: 404, message: { message: 'Car not found' } };
+    const newMotorcycles = motorcycles ? new MotorcyclesDomains(motorcycles) : null;
+    return { status: 200, message: newMotorcycles };
   }
 }
 

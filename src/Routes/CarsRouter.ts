b846fb3c6bd2b6aa96367051dfc62829b50d58d4
Priscila <priscilasartori2@gmail.com>
2023-04-28 23:31:49 -0300

    feat: Adicionando o router de /cars e /cars/:id

--- src/Routes/CarsRouter.ts
+++ src/Routes/CarsRouter.ts
@@ -8,4 +8,14 @@ carRoute.post(
   (req, res, next) => new CarsConstroller(req, res, next).create(),
 );
 
+carRoute.get(
+  '/cars',
+  (req, res, next) => new CarsConstroller(req, res, next).getAll(),
+);
+
+carRoute.get(
+  '/cars/:id',
+  (req, res, next) => new CarsConstroller(req, res, next).getById(),
+);
+
 export default carRoute;

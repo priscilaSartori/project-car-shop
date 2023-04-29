0a95b7c937ad41721dd2b79845824f39a88fc1b1
Priscila <priscilasartori2@gmail.com>
2023-04-29 16:50:14 -0300

    feat: Adicionando o service para atualizar motorcycles

--- src/Services/MotorcyclesServices.ts
+++ src/Services/MotorcyclesServices.ts
@@ -31,6 +31,18 @@ class MotorcyclesService {
     const newMotorcycles = motorcycles ? new MotorcyclesDomains(motorcycles) : null;
     return { status: 200, message: newMotorcycles };
   }
+
+  public async update(id: string, motorcycles: IMotorcycle) {
+    const motorcyclesModel = new MotorcyclesModel();
+    if (!isValidObjectId(id)) return { status: 422, message: { message: 'Invalid mongo id' } };
+    const motorcyclesUpdate = await motorcyclesModel.update(id, motorcycles);
+    if (motorcyclesUpdate === null) {
+      return { 
+        status: 404, message: { message: 'Motorcycle not found' } };
+    }
+    const newMotorcycles = motorcyclesUpdate ? new MotorcyclesDomains(motorcycles) : null;
+    return { status: 200, message: newMotorcycles };
+  }
 }
 
 export default MotorcyclesService;

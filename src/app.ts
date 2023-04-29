414b87db23b01fecc2ca4879631eb9a42df026e1
Priscila <priscilasartori2@gmail.com>
2023-04-29 16:00:17 -0300

    build: Adicionando o router de motorcycles em app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import CarsRouter from './Routes/CarsRouter';
+import MotorcyclesRoute from './Routes/MotorcyclesRoute';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', CarsRouter);
+app.use('/motorcycles', MotorcyclesRoute);
 
 export default app;

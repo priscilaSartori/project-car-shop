168842d1bd1c3effd8e9ce9f018e3a35fd24c54f
Priscila <priscilasartori2@gmail.com>
2023-04-29 16:23:46 -0300

    feat: Adicionando o router get de motorcycles

--- src/Routes/MotorcyclesRoute.ts
+++ src/Routes/MotorcyclesRoute.ts
@@ -4,5 +4,10 @@ import MotorcyclesConstroller from '../Controllers/MotorcyclesController';
 const MotorcyclesRoute = Router();
 
 MotorcyclesRoute.post('/', (req, res, next) => new MotorcyclesConstroller(req, res, next).create());
+MotorcyclesRoute.get('/', (req, res, next) => new MotorcyclesConstroller(req, res, next).getAll());
+MotorcyclesRoute.get(
+  '/:id',
+  (req, res, next) => new MotorcyclesConstroller(req, res, next).getById(),
+);
 
 export default MotorcyclesRoute;

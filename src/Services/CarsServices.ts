af6605d81037bdec461866d6e00cfffc29872740
Priscila <priscilasartori2@gmail.com>
2023-04-28 23:35:22 -0300

    feat: Adicionando a service de /cars e /cars/:id

--- src/Services/CarsServices.ts
+++ src/Services/CarsServices.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import CarsDomains from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarsModel from '../Models/CarsModel';
@@ -10,6 +11,25 @@ class CarsService {
       return new CarsDomains(car);
     }
     return null;
+  }
+
+  public async getAll() {
+    const carsModel = new CarsModel();
+    const cars = await carsModel.getAll();
+    const carsArray = cars.map((car) => {
+      if (car) return new CarsDomains(car);
+      return null;
+    }); 
+    return { status: 200, message: carsArray };
+  }
+
+  public async getById(id: string) {
+    const carsModel = new CarsModel();
+    if (!isValidObjectId(id)) return { status: 422, message: { message: 'Invalid mongo id' } };
+    const cars = await carsModel.getById(id);
+    if (cars === null) return { status: 404, message: { message: 'Car not found' } };
+    const newCars = cars ? new CarsDomains(cars) : null;
+    return { status: 200, message: newCars };
   }
 }
 
